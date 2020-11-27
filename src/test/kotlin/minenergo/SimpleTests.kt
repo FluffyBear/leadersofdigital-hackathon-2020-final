65a46f3aec2dee607fafc1fdc94f9483e867766f
Aleksandr Maksimenko <xe,frrjdfybT8>
2020-11-27 22:52:40 +0300

    simple tests

---
+++ src/test/kotlin/minenergo/SimpleTests.kt
@@ -0,0 +1,21 @@
+package minenergo
+
+import org.junit.Test
+
+class SimpleTests {
+
+    @Test
+    fun oneBigIndustry() {
+
+    }
+
+    @Test
+    fun oneBigIndustryPlusSmallIndustry() {
+
+    }
+
+    @Test
+    fun twoBigIndustries() {
+
+    }
+}
