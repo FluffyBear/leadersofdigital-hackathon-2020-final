7b5e4a5509863e700f5387ff75ffc25022693fb6
Aleksandr Maksimenko <xe,frrjdfybT8>
2020-11-28 17:41:03 +0300

    linear interpolation

--- src/main/kotlin/minenergo/web/Interpolation.kt
+++ src/main/kotlin/minenergo/web/Interpolation.kt
@@ -3,6 +3,7 @@ package minenergo.web
 import minenergo.Industry
 import minenergo.misc.YearMonthProgression
 import org.springframework.stereotype.Component
+import java.time.YearMonth
 
 @Component
 class Interpolation {
@@ -22,15 +23,40 @@ class Interpolation {
     }
 
     private fun fillGaps(industry: Industry, yearMonthProgression: YearMonthProgression) {
-        val value = calculateValue(industry)
+        val dates = industry.power.keys.toList()
+        for (i in 0 until dates.size - 1) {
+            fillRange(industry, YearMonthProgression(dates[i], dates[i + 1]))
+        }
+        fillLast(
+            industry,
+            YearMonthProgression(dates[dates.size - 1], dates.last()),
+            yearMonthProgression.endInclusive
+        )
+    }
+
+    fun fillRange(industry: Industry, yearMonthProgression: YearMonthProgression) {
         yearMonthProgression.forEach {
-            if (!industry.power.keys.contains(it)) {
-                industry.power[it] = value
-            }
+            industry.power[it] = industry.power[yearMonthProgression.start]?.plus(
+                (industry.power[yearMonthProgression.endInclusive]?.minus(
+                    industry.power[yearMonthProgression.start] as Double
+                )!!).div(
+                    (yearMonthProgression.endInclusive.year * 12 + yearMonthProgression.endInclusive.monthValue) -
+                            (yearMonthProgression.start.year * 12 + yearMonthProgression.start.monthValue)
+                )
+            )
         }
     }
 
-    fun calculateValue(industry: Industry): Double {
-        return industry.power.values.average()
+    fun fillLast(industry: Industry, yearMonthProgression: YearMonthProgression, horizon: YearMonth) {
+        YearMonthProgression(yearMonthProgression.start, horizon).forEach {
+            industry.power[it] = industry.power[yearMonthProgression.start]?.plus(
+                (industry.power[yearMonthProgression.endInclusive]?.minus(
+                    industry.power[yearMonthProgression.start] as Double
+                )!!).div(
+                    (yearMonthProgression.endInclusive.year * 12 + yearMonthProgression.endInclusive.monthValue) -
+                            (yearMonthProgression.start.year * 12 + yearMonthProgression.start.monthValue)
+                )
+            )
+        }
     }
 }
