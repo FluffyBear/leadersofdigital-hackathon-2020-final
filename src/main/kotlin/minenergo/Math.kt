df22fe00d54daf5136da0ffe11f269bce5b8b4f4
Vladimir Maksimenko <vladimir.maksimenko@mynd.co>
2020-11-28 12:26:48 +0300

    mergefix

--- src/main/kotlin/minenergo/Math.kt
+++ src/main/kotlin/minenergo/Math.kt
@@ -18,22 +18,18 @@ class Math {
         return OLSMultipleLinearRegression().let { regression ->
             regression.newSampleData(
                 industryToPredict.power.map { it.value }.toDoubleArray(),
-                industries.first().let { industry ->
-                    industry.power.firstKey().rangeTo(industryToPredict.power.lastKey()).map { yearMonth ->
-                        industries.map { it.power.getValue(yearMonth) }.toDoubleArray()
-                    }.toTypedArray()
-                }
+                industries.first().power.firstKey().rangeTo(industryToPredict.power.lastKey()).map { yearMonth ->
+                    industries.map { it.power.getValue(yearMonth) }.toDoubleArray()
+                }.toTypedArray()
             )
-            regression.estimateRegressionParameters().let { params ->
-                Industry(
-                    name = industryToPredict.name,
-                    power = industryToPredict.power.firstKey().rangeTo(predictionHorizon).associateWith { yearMonth ->
-                        (industryToPredict.power[yearMonth] ?: params.reduceIndexed { idx, acc, p ->
-                            acc + p * industries[idx - 1].power.getValue(yearMonth)
-                        })
-                    }.toSortedMap()
-                )
-            }
+            Industry(
+                name = industryToPredict.name,
+                power = industryToPredict.power.firstKey().rangeTo(predictionHorizon).associateWith { yearMonth ->
+                    (industryToPredict.power[yearMonth] ?: regression.estimateRegressionParameters().reduceIndexed { idx, acc, p ->
+                        acc + p * industries[idx - 1].power.getValue(yearMonth)
+                    })
+                }.toSortedMap()
+            )
         }
     }
 
